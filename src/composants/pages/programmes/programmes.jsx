746929f9d6a1f1deb6ce5e2580c20fe6ddf110ef
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2022-12-16 20:34:40 +0100

    corrections de mise en forme sur les pages offres, programmes et actuallités

--- src/composants/pages/programmes/programmes.jsx
+++ src/composants/pages/programmes/programmes.jsx
@@ -13,7 +13,7 @@ function Programmes() {
       <AsideSug 
       element1={<LecteurVideo id="lecteur1" text="DIE HARD 10" />}
       element2={<LecteurVideo id="lecteur2" text="MAD MAX 7" />}
-      element3={<LecteurVideo id="lecteur3" text="LES FANTOMES DE NOEL" />}
+      element3={<LecteurVideo id="lecteur3" text="NOEL EN FAMILLE" />}
       element4={<LecteurVideo id="lecteur4" text="SELECTION YOUTUBE" />}/>
     </div>
   );
